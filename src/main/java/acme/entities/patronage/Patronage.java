ad3df84b17d92a2fc201c325fc8ffc785b2f2111
borpincal <borpincal@alum.us.es>
2022-03-15 18:25:38 +0100

    Task 055: done

--- src/main/java/acme/entities/patronage/Patronage.java
+++ src/main/java/acme/entities/patronage/Patronage.java
@@ -1,10 +1,13 @@
 package acme.entities.patronage;
 
-import java.time.LocalDateTime;
+import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
@@ -33,26 +36,34 @@ public class Patronage extends AbstractEntity{
 	@NotNull
 	protected Status			status;
 	
-	@NotNull
+	@NotBlank
 	@Pattern(regexp="^[A-Z]{3}-[0-9]{3}(-[A-Z])?$")
 	@Column(unique=true)
 	protected String 			code;
 
 	@NotBlank
-	@Length(min=0, max=256)
+	@Length(min=0, max=255)
 	protected String			legalStuff;
 	
 	@NotNull
+	@Valid
 	protected Money				budget;
 	
 	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
 	@Past
-	protected LocalDateTime		creationMoment;
+	protected Date				creationMoment;
 	
 	@NotNull
-	protected LocalDateTime		periodOfTime;
+	@Temporal(TemporalType.TIMESTAMP)
+	protected Date				startTime;
 	
 	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
+	protected Date				endTime;
+	
+	@NotNull
+	@Valid
 	protected Money				retailPrice;
 	
 	@URL
