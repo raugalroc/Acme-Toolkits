2403ba8638e0c3a910d34b6602271aeb9377c79d
Hesusu-ikie <jespaepae@alum.us.es>
2022-03-18 20:17:24 +0100

    Task 040: in progress

--- src/main/java/acme/entities/inventions/Invention.java
+++ src/main/java/acme/entities/inventions/Invention.java
@@ -4,7 +4,6 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.validation.Valid;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
@@ -46,14 +45,13 @@ public class Invention extends AbstractEntity {
 	@Length(min = 0, max = 255)
 	protected String			description;
 	
-	@Min(0)
 	@Valid
 	protected Money				retailPrice;
 	
 	@URL
 	protected String			link;
 	
-	protected InventionType			type;
+	protected InventionType			inventionType;
 	
 	// Derived attributes ----------------------------------------------
 	
