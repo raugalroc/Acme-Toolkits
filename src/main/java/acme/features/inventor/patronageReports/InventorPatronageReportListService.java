25d353a8d17f2b534ad6397329ab6ede13c94f3f
Raúl Gallardo <raulgaro232001@gmail.com>
2022-04-13 20:16:17 +0200

    Task 074: done

---
+++ src/main/java/acme/features/inventor/patronageReports/InventorPatronageReportListService.java
@@ -0,0 +1,54 @@
+package acme.features.inventor.patronageReports;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.entities.patronageReports.PatronageReport;
+import acme.framework.components.models.Model;
+import acme.framework.controllers.Request;
+import acme.framework.services.AbstractListService;
+import acme.roles.Inventor;
+
+@Service
+public class InventorPatronageReportListService implements AbstractListService<Inventor, PatronageReport>{
+	
+	// Internal state ---------------------------------------------------------
+
+		@Autowired
+		protected InventorPatronageReportRepository repository;
+		
+		
+	// AbstractListService<Inventor, PatrongeReport> interface -------------------
+
+
+		@Override
+		public boolean authorise(final Request<PatronageReport> request) {
+			assert request != null;
+
+			return true;
+		}
+		
+		@Override
+		public Collection<PatronageReport> findMany(final Request<PatronageReport> request) {
+			assert request != null;
+
+			Collection<PatronageReport> result;
+			int inventorId;
+
+			inventorId = request.getPrincipal().getActiveRoleId();
+			result = this.repository.findAllPatronageReportsByInventorId(inventorId);
+
+			return result;
+		}
+		
+		@Override
+		public void unbind(final Request<PatronageReport> request, final PatronageReport entity, final Model model) {
+			assert request != null;
+			assert entity != null;
+			assert model != null;
+
+			request.unbind(entity, model, "sequenceNumber", "creationMoment", "memorandum");		
+		}
+}
--- src/main/webapp/WEB-INF/views/fragments/menu.jsp
+++ src/main/webapp/WEB-INF/views/fragments/menu.jsp
@@ -57,7 +57,10 @@
 		<acme:menu-option code="master.menu.inventor" access="hasRole('Inventor')">
 			<acme:menu-suboption code="master.menu.inventor.invention.list-tools" action="/inventor/invention/list-mine?type=TOOL"/>
 			<acme:menu-suboption code="master.menu.inventor.invention.list-components" action="/inventor/invention/list-mine?type=COMPONENT"/>
+			<acme:menu-suboption code="master.menu.inventor.patronageReport" action="/inventor/patronageReport/list"/>
 		</acme:menu-option>
+		
+		
 	</acme:menu-left>
 
 	<acme:menu-right>
