c21e106c13eb5e00471c929fa42da977f4f8ccd7
Hesusu-ikie <jespaepae@alum.us.es>
2022-04-18 23:02:52 +0200

    Task 064: done

---
+++ src/main/java/acme/features/any/chirp/AnyChirpRepository.java
@@ -0,0 +1,21 @@
+package acme.features.any.chirp;
+
+import java.util.Collection;
+import java.util.Date;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import acme.entities.chirps.Chirp;
+import acme.framework.repositories.AbstractRepository;
+
+@Repository
+public interface AnyChirpRepository extends AbstractRepository{
+	
+	@Query("select c from Chirp c where c.creationMoment > :deadline")
+	Collection<Chirp> findRecentChirp(Date deadline);
+	
+	@Query("select c from Chirp c where c.id = :id")
+	Chirp findOneChirpById(int id);
+	
+}
