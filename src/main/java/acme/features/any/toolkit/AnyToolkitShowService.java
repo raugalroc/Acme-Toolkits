52d49b5e82efd4368f80cf29ae45e4ec51f8fc0a
Hesusu-ikie <jespaepae@alum.us.es>
2022-04-22 13:16:35 +0200

    Task 067: updated

--- src/main/java/acme/features/any/toolkit/AnyToolkitShowService.java
+++ src/main/java/acme/features/any/toolkit/AnyToolkitShowService.java
@@ -1,7 +1,11 @@
 package acme.features.any.toolkit;
 
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import acme.entities.inventions.Invention;
 import acme.entities.toolkits.Toolkit;
 import acme.framework.components.models.Model;
 import acme.framework.controllers.Request;
@@ -11,21 +15,36 @@ import acme.framework.services.AbstractShowService;
 @Service
 public class AnyToolkitShowService implements AbstractShowService<Any, Toolkit>{
 
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	protected AnyToolkitRepository repository;
+	
+	
 	@Override
 	public boolean authorise(final Request<Toolkit> request) {
-		// TODO Auto-generated method stub
-		return false;
+		assert request != null;
+		return true;
 	}
 
 	@Override
 	public Toolkit findOne(final Request<Toolkit> request) {
-		// TODO Auto-generated method stub
-		return null;
+		final int id = request.getModel().getInteger("id");
+		return this.repository.findOneToolkitById(id);
 	}
 
 	@Override
 	public void unbind(final Request<Toolkit> request, final Toolkit entity, final Model model) {
-		// TODO Auto-generated method stub
+		assert request != null;
+		assert entity != null;
+		assert model != null;
+		final Double totalPrice = this.repository.findTotalRetailPriceByToolkitId(entity.getId());
+		final Collection<Invention> inventions = this.repository.findManyInventionsByToolkitId(entity.getId());
+		//TODO sumar todos los precios en base a la moneda actual que tiene el usuario asignada.
+		model.setAttribute("totalPrice", totalPrice);
+		model.setAttribute("id", entity.getId());
+		request.unbind(entity, model, "code", "title", "description","assemblyNotes","link");
+		
 		
 	}
 
