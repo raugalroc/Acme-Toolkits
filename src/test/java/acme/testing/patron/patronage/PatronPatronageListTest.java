62c7e2989e3bc28a98020721dea820b3c0b0e95e
Raúl Gallardo <raulgaro232001@gmail.com>
2022-04-23 15:16:32 +0200

    Task 089: done

---
+++ src/test/java/acme/testing/patron/patronage/PatronPatronageListTest.java
@@ -0,0 +1,36 @@
+package acme.testing.patron.patronage;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import acme.testing.TestHarness;
+
+public class PatronPatronageListTest extends TestHarness{
+	
+	@ParameterizedTest
+	@CsvFileSource(resources = "/patron/patronage/list.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(10)
+	public void positiveTestPatronageAsPatron(final int recordIndex, final String status, final String code, 
+		final String legalStuff, final String budget, final String creationTime, final String startTime,
+		final String endTime) {
+		
+		super.signIn("patron1", "patron1");
+
+		super.clickOnMenu("Patron", "Patronage List");
+		super.checkListingExists();
+	
+		super.checkColumnHasValue(recordIndex, 0, status);
+		super.checkColumnHasValue(recordIndex, 1, code);
+		super.checkColumnHasValue(recordIndex, 2, budget);
+		super.checkColumnHasValue(recordIndex, 3, legalStuff);
+		super.checkColumnHasValue(recordIndex, 4, creationTime);
+		super.checkColumnHasValue(recordIndex, 5, startTime);
+		super.checkColumnHasValue(recordIndex, 6, endTime);
+		
+		
+
+		super.signOut();
+	}
+
+}
---
+++ src/test/java/acme/testing/patron/patronage/PatronPatronageShowTest.java
@@ -0,0 +1,38 @@
+package acme.testing.patron.patronage;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import acme.testing.TestHarness;
+
+public class PatronPatronageShowTest extends TestHarness{
+	
+	@ParameterizedTest
+	@CsvFileSource(resources = "/patron/patronage/show.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(10)
+	public void positiveTestPatronageAsPatron(final int recordIndex, final String status, final String code, 
+		final String legalStuff, final String budget, final String creationTime, final String startTime,
+		final String endTime, final String link) {
+		
+		super.signIn("patron1", "patron1");
+
+		super.clickOnMenu("Patron", "Patronage List");
+
+		super.clickOnListingRecord(recordIndex);
+		super.checkFormExists();
+		super.checkInputBoxHasValue("status", status);
+		super.checkInputBoxHasValue("code", code);
+		super.checkInputBoxHasValue("legalStuff", legalStuff);
+		super.checkInputBoxHasValue("budget", budget);
+		super.checkInputBoxHasValue("creationTime", creationTime);
+		super.checkInputBoxHasValue("startTime", startTime);
+		super.checkInputBoxHasValue("endTime", endTime);
+		super.checkInputBoxHasValue("link", link);
+		
+		
+
+		super.signOut();
+	}
+
+}
